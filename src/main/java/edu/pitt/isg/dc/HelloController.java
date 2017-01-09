589a9340639acf38b1777b58b1cb65e0e7e4facf
Adam Darr <amd176@pitt.edu>
2017-01-09 17:05:54 -0500

    Add Waiting Time to Infectious Disease Emergence to publication listing

--- src/main/java/edu/pitt/isg/dc/HelloController.java
+++ src/main/java/edu/pitt/isg/dc/HelloController.java
@@ -25,7 +25,8 @@ public class HelloController {
     private List<Map<String, Object>> dummyModels() {
         final ArrayList<Map<String, Object>> list = new ArrayList<>();
         list.add(dummyDataAugmentedPublication().toBootstrapTree());
-        System.out.println(list);
+        list.add(dummyDataAugmentedPublication("Waiting time to infectious disease emergence",
+                dummyFromScratch("Christopher J. Dibble, Eamon B. O'Dea, Andrew W. Park, John M. Drake", "10-19-2016", "Waiting time to infectious disease emergence", "10.1098/rsif.2016.0540", "Paper", "The Royal Society", "http://dx.doi.org/10.1098/rsif.2016.0540"), dummyFromScratch("Dibble CJ, O'Dea EB, Park AW, Drake JM", "10-27-2016", "Data from: Waiting time to infectious disease emergence", "10.5061/dryad.dp4kb", "Dryad Data Package", "Dryad Digital Repository", "http://dx.doi.org/10.5061/dryad.dp4kb")).toBootstrapTree());
         return list;
     }
 
@@ -34,6 +35,14 @@ public class HelloController {
         dataAugmentedPublication.setName("Ebola Cases and Health System Demand in Liberia");
         dataAugmentedPublication.setPaper(dummyPaper());
         dataAugmentedPublication.setData(dummyAugmentedData());
+        return dataAugmentedPublication;
+    }
+
+    private DataAugmentedPublication dummyDataAugmentedPublication(String title, Publication paper, Publication data) {
+        final DataAugmentedPublication dataAugmentedPublication = new DataAugmentedPublication();
+        dataAugmentedPublication.setName(title);
+        dataAugmentedPublication.setPaper((Paper) paper);
+        dataAugmentedPublication.setData((AugmentedData) data);
         return dataAugmentedPublication;
     }
 
@@ -54,7 +63,7 @@ public class HelloController {
         data.setName("Data from: Ebola cases and health system demand in Liberia");
         data.setDoi("10.5061/dryad.17m5q");
         data.setTypeText("Dryad Data Package");
-        data.setJournal("PLOS Biology");
+        data.setJournal("Dryad Digital Repository");
         data.setUrl("http://dx.doi.org/10.5061/dryad.17m5q");
         return data;
     }
@@ -62,5 +71,22 @@ public class HelloController {
     private void fillDummyEbola(Publication data) {
         data.setAuthorsText("Drake JM, Kaul RB, Alexander LW, O'Regan SM, Kramer AM, Pulliam JT, Ferrari MJ, Park AW");
         data.setPublicationDateText("1-13-2015");
+    }
+
+    private Publication dummyFromScratch(String author, String date, String name, String doi, String typeText, String journal, String url) {
+        Publication data = new AugmentedData();
+
+        if(typeText.equals("Paper")) {
+            data = new Paper();
+        }
+
+        data.setAuthorsText(author);
+        data.setPublicationDateText(date);
+        data.setName(name);
+        data.setDoi(doi);
+        data.setTypeText(typeText);
+        data.setJournal(journal);
+        data.setUrl(url);
+        return data;
     }
 }
