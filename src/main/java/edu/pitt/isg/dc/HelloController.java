3276d00a72111de57917c21d771ad8a0e1251b62
mas400 <mas400@mas400-mdt.dbmi.pitt.edu>
2017-01-10 11:29:56 -0500

    updated with query to library viewer

--- src/main/java/edu/pitt/isg/dc/HelloController.java
+++ src/main/java/edu/pitt/isg/dc/HelloController.java
@@ -1,5 +1,6 @@
 package edu.pitt.isg.dc;
 
+import edu.pitt.isg.dc.Utils.DigitalCommonsProperties;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,11 +8,21 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.Properties;
+
 @Controller
 public class HelloController {
+    private static String VIEWER_URL = "";
+
+    static {
+        Properties configurationProperties = DigitalCommonsProperties.getProperties();
+        VIEWER_URL = configurationProperties.getProperty(DigitalCommonsProperties.LIBRARY_VIEWER_URL);
+    }
+
     @RequestMapping(value = "/home", method = RequestMethod.GET)
     public String hello(Model model, @RequestParam(value="name", required=false, defaultValue="World") String name) {
 
+        model.addAttribute("libraryViewerUrl", VIEWER_URL);
         return "commons";
     }
 
