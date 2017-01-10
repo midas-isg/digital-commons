b7cee095145f1d2df756be81ecc2f20c793a8500
sahawut <sahawut@gmail.com>
2017-01-10 16:26:53 -0500

    Cleaned up.

--- src/main/java/edu/pitt/isg/dc/digital/Digital.java
+++ src/main/java/edu/pitt/isg/dc/digital/Digital.java
@@ -14,9 +14,6 @@ import javax.persistence.ManyToOne;
 public class Digital {
     private Long id;
     private String name;
-    private String typeText;
-    private String doi;
-    private String url;
 
     @Id
     @GeneratedValue(strategy= GenerationType.AUTO)
@@ -39,29 +36,5 @@ public class Digital {
     @Override
     public String toString() {
         return "id=" + id + ", name='" + name + "'";
-    }
-
-    public String getDoi() {
-        return doi;
-    }
-
-    public void setDoi(String doi) {
-        this.doi = doi;
-    }
-
-    public String getUrl() {
-        return url;
-    }
-
-    public void setUrl(String url) {
-        this.url = url;
-    }
-
-    public String getTypeText() {
-        return typeText;
-    }
-
-    public void setTypeText(String typeText) {
-        this.typeText = typeText;
     }
 }
