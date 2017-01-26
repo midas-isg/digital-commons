fc3a6f36b7208a7a0df7a05988490e3826fb0d8a
mas400 <mas400@mas400-mdt.dbmi.pitt.edu>
2017-01-26 11:57:16 -0500

    updated publications page and sorted software and standards on main page

--- src/main/java/edu/pitt/isg/dc/digital/software/SoftwareRepository.java
+++ src/main/java/edu/pitt/isg/dc/digital/software/SoftwareRepository.java
@@ -3,5 +3,8 @@ package edu.pitt.isg.dc.digital.software;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 public interface SoftwareRepository extends CrudRepository<Software, Long> {
+    public List<Software> findAllByOrderByName();
 }
