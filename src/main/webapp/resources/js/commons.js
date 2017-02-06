bb3913134117f33ee83ac39460382d30ea5ef5c4
Adam Darr <amd176@pitt.edu>
2017-02-06 10:30:17 -0500

    Update hardcoded software

--- src/main/webapp/resources/js/commons.js
+++ src/main/webapp/resources/js/commons.js
@@ -524,9 +524,9 @@ function getPopover(imgPath, title, modalImgPath, softwareName) {
 }
 
 function compareNodes(a,b) {
-    if (a.name < b.name)
+    if (a.name.toLowerCase() < b.name.toLowerCase())
         return -1;
-    if (a.name > b.name)
+    if (a.name.toLowerCase() > b.name.toLowerCase())
         return 1;
     return 0;
 }
