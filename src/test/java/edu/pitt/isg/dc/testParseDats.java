836c5b34a983085dfae13f0cd1b3f62fddc03878
Adam Darr <adamdarr@gmail.com>
2017-06-05 11:15:24 -0400

    Add methods to populate datastore

--- src/test/java/edu/pitt/isg/dc/testParseDats.java
+++ src/test/java/edu/pitt/isg/dc/testParseDats.java
@@ -1,31 +1,16 @@
 package edu.pitt.isg.dc;
 
 
-import com.google.gson.JsonObject;
 import edu.pitt.isg.JsonConverter;
-import edu.pitt.isg.dc.entry.classes.EntryObject;
-import edu.pitt.isg.dc.entry.impl.JsonMdcEntryDatastoreImpl;
 import edu.pitt.isg.mdc.dats2_2.Dataset;
-import org.apache.commons.codec.binary.Base64;
 import org.apache.commons.io.FileUtils;
-import org.apache.commons.io.IOUtils;
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.NameValuePair;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.HttpClients;
-import org.apache.http.message.BasicNameValuePair;
 import org.junit.Test;
 
 import java.io.File;
 import java.io.FileFilter;
 import java.io.IOException;
-import java.io.InputStream;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Iterator;
-import java.util.List;
 
 import static junit.framework.TestCase.assertTrue;
 import static org.junit.Assert.fail;
