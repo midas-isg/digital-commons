83ae92071faf4c528d753f7f9022b75fea1de59c
Adam Darr <adamdarr@gmail.com>
2017-06-05 14:02:57 -0400

    More refactoring

--- src/main/java/edu/pitt/isg/dc/entry/PopulateDatastore.java
+++ src/main/java/edu/pitt/isg/dc/entry/PopulateDatastore.java
@@ -3,30 +3,43 @@ package edu.pitt.isg.dc.entry;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import edu.pitt.isg.dc.entry.classes.EntryObject;
+import edu.pitt.isg.dc.entry.exceptions.MdcEntryDatastoreException;
 import edu.pitt.isg.dc.entry.impl.H2Datastore;
 import edu.pitt.isg.dc.entry.interfaces.MdcEntryDatastoreInterface;
+import edu.pitt.isg.dc.utils.DigitalCommonsProperties;
 import org.apache.commons.io.FileUtils;
 
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Paths;
 import java.util.List;
+import java.util.Properties;
 
 public class PopulateDatastore {
-    private final String entriesFilepath = "/Users/amd176/Documents/mdc-data";
+    private static String ENTRIES_FILEPATH = "";
 
-    public PopulateDatastore() throws Exception {
+    static {
+        Properties configurationProperties = DigitalCommonsProperties.getProperties();
+        ENTRIES_FILEPATH = configurationProperties.getProperty(DigitalCommonsProperties.ENTRIES_FILEPATH);
+    }
+
+    public PopulateDatastore() throws MdcEntryDatastoreException {
         this.populate();
     }
 
-    public void populate() throws Exception {
+    public void populate() throws MdcEntryDatastoreException {
         MdcEntryDatastoreInterface datastore = new H2Datastore();
         String[] extensions = new String[] { "json" };
 
         JsonParser parser = new JsonParser();
-        List<File> files = (List<File>) FileUtils.listFiles(Paths.get(entriesFilepath).toFile(), extensions, true);
+        List<File> files = (List<File>) FileUtils.listFiles(Paths.get(ENTRIES_FILEPATH).toFile(), extensions, true);
         for(File file : files) {
-            String jsonEntryString = FileUtils.readFileToString(file, "UTF-8");
+            String jsonEntryString = null;
+            try {
+                jsonEntryString = FileUtils.readFileToString(file, "UTF-8");
+            } catch (IOException e) {
+                throw new MdcEntryDatastoreException(e);
+            }
             JsonObject jsonEntryObject = parser.parse(jsonEntryString).getAsJsonObject();
 
             EntryObject entryObject = new EntryObject();
