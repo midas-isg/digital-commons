ea72713778b31bf7a05bd3f505063292acfcb433
mas400 <mas400@mas400-mdt.dbmi.pitt.edu>
2018-05-31 17:06:01 -0400

    updated custom editor to now remove trailing commas from string inputs

--- src/main/java/edu/pitt/isg/dc/validator/CustomDatasetEditor.java
+++ src/main/java/edu/pitt/isg/dc/validator/CustomDatasetEditor.java
@@ -4,10 +4,11 @@ import java.beans.PropertyEditorSupport;
 
 public class CustomDatasetEditor extends PropertyEditorSupport{
     public void setAsText(String text) {
-        if(text.equals(",")) {
-            setValue(null);
-        } else {
-            setValue(text);
-        }
+        setValue(text.replaceAll("(,)*$", ""));
+//        if(text.equals(",")) {
+//            setValue(null);
+//        } else {
+//            setValue(text);
+//        }
     }
 }
