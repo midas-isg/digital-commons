f1864f3b12aaa7ad1e60a525f1690f9342aceee2
Stazer <jbs82@dbmi-nb-117.dbmi.pitt.edu>
2018-07-24 14:43:14 -0400

    AutoPopulating fix for IsAbout

--- src/main/java/edu/pitt/isg/dc/utils/AutoPopulatedWrapper/AutoPopulatedIsAboutWrapper.java
+++ src/main/java/edu/pitt/isg/dc/utils/AutoPopulatedWrapper/AutoPopulatedIsAboutWrapper.java
@@ -1,12 +1,13 @@
 package edu.pitt.isg.dc.utils.AutoPopulatedWrapper;
 
+import edu.pitt.isg.dc.entry.classes.IsAboutItems;
 import edu.pitt.isg.dc.utils.DatasetFactory;
 import edu.pitt.isg.mdc.dats2_2.IsAbout;
 
-public class AutoPopulatedIsAboutWrapper extends IsAbout {
+public class AutoPopulatedIsAboutWrapper extends IsAboutItems {
     public AutoPopulatedIsAboutWrapper() {
 
         super();
-        DatasetFactory.createIsAbout(this);
+        DatasetFactory.createIsAboutItems(this);
     }
 }
