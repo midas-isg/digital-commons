54aeb713c3f089eba07fc4a75a7b5a05e63c8296
JohnLevander <jdl50@pitt.edu>
2018-07-25 11:14:14 -0400

    updating reflection factory

--- src/main/java/edu/pitt/isg/dc/utils/ReflectionFactory.java
+++ src/main/java/edu/pitt/isg/dc/utils/ReflectionFactory.java
@@ -6,37 +6,10 @@ import org.springframework.util.AutoPopulatingList;
 import java.lang.reflect.Method;
 import java.util.*;
 
-
-//personComprisedEntity needs to be PersonOrganization
-//isAbout section, they must be isAboutItems not Annotations or BiologicalEntities
-//make everything AutoPopulatedWrapper
 public class ReflectionFactory {
 
-    private static Set<Class<?>> isPrimitveWrapper() {
-        Set<Class<?>> ret = new HashSet<Class<?>>();
-        ret.add(Boolean.class);
-        ret.add(Character.class);
-        ret.add(Byte.class);
-        ret.add(Short.class);
-        ret.add(Integer.class);
-        ret.add(Long.class);
-        ret.add(Float.class);
-        ret.add(Double.class);
-        ret.add(Void.class);
-        ret.add(String.class);
-        return ret;
-    }
-
-    private static Class getClass(String className) throws ClassNotFoundException {
-        try {
-            return Class.forName("edu.pitt.isg.dc.utils.AutoPopulatedWrapper.AutoPopulated" + className.substring(className.lastIndexOf(".") + 1, className.length()) + "Wrapper");
-        } catch (ClassNotFoundException e) {
-            return Class.forName(className);
-        }
-    }
-
     private static String getGenericTypeOfDeclaredField(String field, Class clazz) {
-        if (field.equals("affiliations")) {
+        if (field.equals("alternateIdentifiers") && clazz.getName().contains("About")) {
             System.out.println("hmm");
         }
         try {
@@ -49,19 +22,37 @@ public class ReflectionFactory {
         }
     }
 
+    private static <Type> List<Type> getGenericList(Class<Type> type, Class clazz) throws Exception {
+        List<Type> l = new ArrayList<Type>();
+        Object newInstance = ReflectionFactory.create(clazz);
+        l.add((Type) newInstance);
+        return new AutoPopulatingList(l, new ReflectionFactoryElementFactory<Type>(clazz));
+    }
+
+    private static List<Method> gatherMethods(Object object, List<Method> methods) throws Exception {
+        if (methods == null) {
+            methods = new ArrayList<>();
+        }
+        methods.addAll(Arrays.asList(object.getClass().getMethods()));
+        if (object.getClass().getSuperclass().getName().equals("java.lang.Object"))
+            return methods;
+        else
+            return gatherMethods(object.getClass().getSuperclass().newInstance(), methods);
+    }
+
+
     public static Object create(Class<?> clazz) throws Exception {
         if (clazz.getName().endsWith("PersonComprisedEntity")) {
             clazz = Class.forName("edu.pitt.isg.dc.entry.classes.PersonOrganization");
         }
-        Object instance = getClass(clazz.getName()).newInstance();
-        //if someone is asking to create a Date, then we actually want to create a AutoPopulatedDateWrapper if...
+        if (clazz.getName().endsWith("IsAbout")) {
+            clazz = Class.forName("edu.pitt.isg.dc.entry.classes.IsAboutItems");
+        }
+        Object instance = clazz.newInstance();
         for (Method m : ReflectionFactory.gatherMethods(instance, null)) {
             if (m.getName().startsWith("get") && m.getParameterTypes().length == 0) {
                 final Object r = m.invoke(instance);
-                if (isPrimitveWrapper().contains(m.getReturnType())) {
-
-                } else {
-                    System.out.println(m.getName() + " = " + r);
+                if (!m.getReturnType().getName().startsWith("java.lang")) {
                     String possibleSetter = "s" + m.getName().substring(1, m.getName().length());
                     try {
                         Method setter = instance.getClass().getMethod(possibleSetter, m.getReturnType());
@@ -86,35 +77,14 @@ public class ReflectionFactory {
                     }
                 }
             }
-
         }
         return instance;
     }
 
-    private static <Type> List<Type> getGenericList(Class<Type> type, Class clazz) throws Exception {
-        List<Type> l = new ArrayList<Type>();
-        Object newInstance = ReflectionFactory.create(clazz);
-        l.add((Type) newInstance);
-        return new AutoPopulatingList(l, clazz);
-    }
-
     public static void main(String[] args) throws Exception {
-
-
         Dataset d = (Dataset) ReflectionFactory.create(Dataset.class);
+        d.getLicenses().get(2);
         System.out.println(d);
-
     }
 
-    public static List<Method> gatherMethods(Object object, List<Method> methods) throws Exception {
-        if (methods == null) {
-            methods = new ArrayList<>();
-        }
-        methods.addAll(Arrays.asList(object.getClass().getMethods()));
-        if (object.getClass().getSuperclass().getName().equals("java.lang.Object"))
-            return methods;
-        else
-            return gatherMethods(object.getClass().getSuperclass().newInstance(), methods);
-
-    }
 }
---
+++ src/main/java/edu/pitt/isg/dc/utils/ReflectionFactoryElementFactory.java
@@ -0,0 +1,36 @@
+package edu.pitt.isg.dc.utils;
+
+import edu.pitt.isg.mdc.dats2_2.Person;
+import org.springframework.util.AutoPopulatingList;
+
+public class ReflectionFactoryElementFactory<E> implements AutoPopulatingList.ElementFactory<E> {
+    private final Class<? extends E> elementClass;
+
+    public ReflectionFactoryElementFactory(Class<? extends E> elementClass) {
+        this.elementClass = elementClass;
+    }
+
+    @Override
+    public E createElement(int index) throws AutoPopulatingList.ElementInstantiationException {
+        try {
+            return (E) ReflectionFactory.create(this.elementClass);
+        }
+        catch (InstantiationException ex) {
+            throw new AutoPopulatingList.ElementInstantiationException(
+                    "Unable to instantiate element class: " + this.elementClass.getName(), ex);
+        }
+        catch (IllegalAccessException ex) {
+            throw new AutoPopulatingList.ElementInstantiationException(
+                    "Could not access element constructor: " + this.elementClass.getName(), ex);
+        } catch (Exception e) {
+            throw new AutoPopulatingList.ElementInstantiationException(
+                    "Other error: " + this.elementClass.getName(), e);
+        }
+    }
+
+    public static void main(String[] args) {
+        ReflectionFactoryElementFactory<Person> factory = new ReflectionFactoryElementFactory<>(Person.class);
+        factory.createElement(3);
+
+    }
+}
